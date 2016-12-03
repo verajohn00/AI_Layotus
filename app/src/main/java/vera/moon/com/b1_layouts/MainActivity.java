a1e611bc7d6034a98589a24be7abac0d94a35149
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2016-12-03 10:46:49 -0600

    Update MainActivity.java

--- app/src/main/java/vera/moon/com/b1_layouts/MainActivity.java
+++ app/src/main/java/vera/moon/com/b1_layouts/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //another change...
+        
+        var = 0;
         
     }
 }
